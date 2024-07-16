3b2db7714526a2aa8522ff0739228d5187c03c36
nicolas schamne <schamne65@gmail.com>
2024-07-16 18:01:18 -0300

    prueba casi aanda

--- prueba/tienda_nube.php
+++ prueba/tienda_nube.php
@@ -11,7 +11,7 @@ checkLogin();
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/style_icon.css">
     
-    <title>Tienda Despacho</title>
+    <title>Tienda Despacho ¿Guada me pondre feliz?</title>
     <style>
         input{width: 80px;margin:0 5px 20px}
     </style>
