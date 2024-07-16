964a98a78389158de0bf61e5ed21bdb1316aedfc
nicolas schamne <schamne65@gmail.com>
2024-07-16 17:39:32 -0300

    prueba casi aanda

--- comunes/session.php
+++ comunes/session.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 function checkLogin() {
     if (!isset($_SESSION['username'])) {
         header('Location: login.php');
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include './comunes/session.php';
 checkLogin();
 ?>
