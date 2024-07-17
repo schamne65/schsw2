aa80b2b389b1bd15b2a75d6fc0e7aa6b0e2a208f
nicolas schamne <schamne65@gmail.com>
2024-07-16 22:46:23 -0300

    login regidiet

--- usuario/login.php
+++ usuario/login.php
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
 </head>
-<body>
+<body class="login">
     <?php
     echo $_SESSION['username'];
     echo $username;
