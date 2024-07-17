258d5723bf30c02c4b148f868b8c60c8916bb57b
unknown <schamne65@gmail.com>
2024-07-17 20:12:05 -0300

    ruta de gi

--- prueba/funciones/insertar.php
+++ prueba/funciones/insertar.php
@@ -38,6 +38,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Error al insertar insumo.";
     }
 }
+ };
+ 
+
+ function despachoTiendaNubeModificado($cliente_id,$tipo_pedido,$cajas_mixtas,$dulce_leche,$dulce_cacao,$mermelada_frutilla,$mermelada_durazno,$nombre_responsable,$codigo,$fecha) {
+    try {
+        $base = conexion();
+        $consulta = "INSERT INTO despacho_tienda_nube (id_cliente,tipo_pedido,caja_mixtas,dulce_leche,dulce_cacao,mermelada_frutilla,mermelada_durazno,nombre_responsable,codigo_barra,fecha_armado) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $base->prepare($consulta);
+        $stmt->execute([$cliente_id,$tipo_pedido,$cajas_mixtas,$dulce_leche,$dulce_cacao,$mermelada_frutilla,$mermelada_durazno,$nombre_responsable,$codigo,$fecha]);
+    
+    return true;
+    
+} catch (PDOException $e) {
+    echo 'aca Error al insertar pedido' . $e->getMessage();
+    return false;
+}
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_POST['tipo'] == 'tienda_nube_modificado') {
+        $cliente_id = $_POST['cliente_id'];
+        $tipo_pedido = $_POST['tipo_pedido'];
+        $cajas_mixtas = $_POST['cajas_mixtas'];
+        $dulce_leche = $_POST['dulce_leche'];
+        $dulce_cacao = $_POST['dulce_cacao'];
+        $mermelada_frutilla = $_POST['mermelada_frutilla'];
+        $mermelada_durazno = $_POST['mermelada_durazno'];
+        $nombre_responsable = $_POST['nombre_responsable']; 
+        $codigo = $_POST['barra']; 
+        $fecha=date("Y-m-d H:i:s");
+
+    
+
+    // Insertar el proveedor en la base de datos
+    if (despachoTiendaNubeModificado($cliente_id,$tipo_pedido,$cajas_mixtas,$dulce_leche,$dulce_cacao,$mermelada_frutilla,$mermelada_durazno,$nombre_responsable,$codigo,$fecha)) {
+        
+    } else {
+        echo "Error al insertar insumo.";
+    }
+}
  }
 
 
--- prueba/tienda_nube.php
+++ prueba/tienda_nube.php
@@ -32,9 +32,44 @@ $_SESSION['LAST_ACTIVITY'] = time();
 <?php  include '../comunes/nav.php';
 include '../db/conexion.php';
 
-?>
+if ($_SESSION['username'] == "schswadmin") {?>
+
 <section>
-<h2>Pedidos Tienda Nube </h2>
+<h2>Pedidos Tienda Nube Modificar valores</h2>
+    <form action="./funciones/insertar.php" method="POST" >
+    <input type="hidden" name="tipo" value="tienda_nube_modificado">
+        <label for="cliente_id">Doc. Cliente</label>
+        <input autofocus type="text" id="cliente_id" name="cliente_id" required >
+        <label for="tipo_pedido">Tipo de pedido</label>
+        <select name="tipo_pedido" id="tipo_pedido">
+            <option value="Cajas estandar" >Cajas tipo estandar</option>
+            <option value="Pedido personalizado"  >Frascos</option>
+        </select>
+        <div>
+        <label for="cajas_mixtas">Caja Mixtas</label>
+        <input type="number" id="cajas_mixtas" value="0" name="cajas_mixtas" required> 
+        <label for="dulce_leche">Dulce de Leche</label>
+       <input type="number" id="dulce_leche" value="0" name="dulce_leche" required> 
+       <label for="dulce_cacao">Dulce de leche con Cacao</label>
+       <input type="number" id="dulce_cacao" value="0" name="dulce_cacao" required> 
+       <label for="mermelada_frutilla">Mermelada de frutilla</label>
+       <input type="number" id="mermelada_frutilla" value="0" name="mermelada_frutilla" required> 
+       <label for="mermelada_durazno">Mermelada de durazno</label>
+       <input type="number" id="mermelada_durazno" value="0" name="mermelada_durazno" required> 
+       </div>
+
+        <label for="nombre_responsable"> Nombre Responsable:</label>
+        <input type="text" id="nombre_responsable" name="nombre_responsable" requerid>
+
+        <label for="barra"> Codigo de barra:</label>
+        <input type="text" id="barra" name="barra" requerid>
+         
+        <input class="guardar" type="submit" value="Guardar">
+</form>
+
+  
+<?php } else { ?>
+    <h2>Pedidos Tienda Nube </h2>
     <form action="./funciones/insertar.php" method="POST" >
     <input type="hidden" name="tipo" value="tienda_nube">
         <label for="cliente_id">Doc. Cliente</label>
@@ -65,6 +100,14 @@ include '../db/conexion.php';
          
         <input class="guardar" type="submit" value="Guardar">
 </form>
+<?php }
+
+
+?>
+
+
+
+
 
 <?php
 
