5daee56456945a7eca02e14386b495b8ba14249a
nicolas schamne <schamne65@gmail.com>
2024-07-18 14:37:48 -0300

    eliminar datos

--- prueba/funciones/insertar.php
+++ prueba/funciones/insertar.php
@@ -100,8 +100,10 @@ function eliminarPedido($cliente_id){
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "entra";
-    if ($_POST=['tipo'] == 'tienda_nube_eliminar') {
-    echo "entra2"
+   
+    if ($_POST['tipo'] == 'tienda_nube_eliminar') {
+
+    echo "entra2";
         $cliente_id = $_POST['eliminar_pedido'];
         echo "entra 2";
         if (eliminarPedido($cliente_id)) {
