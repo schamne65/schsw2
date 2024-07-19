3127dc34c7db85ce32689f6478d5c07ea45e0ef5
nicolas schamne <schamne65@gmail.com>
2024-07-19 12:21:48 -0300

    descargar atras

--- db/conexion.php
+++ db/conexion.php
@@ -2,7 +2,7 @@
 
 function conexion() {
      try {
-         $base = new PDO('mysql:host=localhost;dbname=c2082289_sch', 'c2082289_sch', 'pilavaBU97');
+         $base = new PDO('mysql:host=localhost;dbname=inventario', 'root', 'mysql');
          $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
          return $base;
      } catch (PDOException $e) {
@@ -10,8 +10,6 @@ function conexion() {
          return null;
      }
  };
-
-
 
 
 
